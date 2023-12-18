2924084c590cba3f52517d86c8ad7289742deea7
ghianlughi <gihanlughibintang@gmail.com>
2023-12-18 12:15:01 +0700

    feat:learn-js

---
+++ data-type/index.js
@@ -0,0 +1,48 @@
+//Data type in javacript
+let a = 5
+let b = "AKU "
+let jeneng = 'KAMU'
+
+let isMale = true
+let isFemale = false
+
+let buah = ['jeruk', 'apel', 'mangga']
+
+let kalimat = "Dia  'Anak baik'"
+let kalimat2 = 'Dia bukam anak baik"'
+
+
+
+console.log(a, b, jeneng, isMale, isFemale, buah, kalimat, kalimat2)
+
+let makanan = ['rice', 'uduk', 'goreng']
+console.log(makanan)
+
+let number = ['100,200,300']
+console.log(number)
+
+let collection = ['KATA KATA', 350, true]
+console.log(collection)
+
+let profile = {
+    nama: "GHIANLUGHI",
+    umur: 18,
+    kota: "KARANGANYAR",
+    isMale: true,
+}
+console.log(profile.nama)
+console.log(profile.umur)
+
+//buat 1 variabel const dengan tipe object dengan nama variabel 'myProfile'
+// ada 4 properti
+//name,age,city,isMale,major
+//feat: add data-typed
+
+const profil = {
+    name: "FITO ANAK BAIK",
+    age: 18,
+    city: "KARANGANYAR",
+    isMale: true,
+    major: "SOFTWARE ENGINERING",
+}
+console.log(profil)
