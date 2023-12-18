c8e3dd2a2f7a2487e8f8b0ae1f4fc0db61769120
ghianlughi <gihanlughibintang@gmail.com>
2023-12-18 14:13:07 +0700

    feat: looping

---
+++ looping/index.js
@@ -0,0 +1,11 @@
+// for ( let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// const buah = ["MANGGA","PEPAYA","DURIAN","ANGGUR"]
+// for (let i = 0; i < buah.length; i++){
+//     console.log("Buah : " + buah[i])
+// }
+const makanan = ["NASI GORENG", "AYAM GEPREK","BAKSO"]
+for (let i = 0; i < makanan.length; i++) {
+    console.log("Fito suka makan : " + makanan[i])
+}
