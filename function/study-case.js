ed67d40bdd3d14bf2fccdaa5cd4b03b6203a7243
ghianlughi <gihanlughibintang@gmail.com>
2023-12-19 12:55:43 +0700

    feat/function

---
+++ function/study-case.js
@@ -0,0 +1,10 @@
+function greetingProgram(nama) {
+    return "hallo selamat datang," + name + ", di CV ZAMAN NOW";
+}
+console.log(greetingProgram(fito));
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(20, 16));
